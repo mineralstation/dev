3e3ae405b31ea14c29eb0025778298782cde24d6
Yang Yang <yangyang4cs@gmail.com>
2020-11-17 00:10:20 -0800

    test

---
+++ com.osgi.example1/docker/node-webserver/index.js
@@ -0,0 +1,18 @@
+// content of index.js
+const http = require('http')
+const port = 80
+
+const requestHandler = (request, response) => {
+	console.log(request.url)
+	response.end('Hello Node.js Server! - ' + new Date().toISOString())
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+	if (err) {
+		return console.log('something bad happened', err)
+	}
+
+	console.log('server is listening on ${port}')
+})
